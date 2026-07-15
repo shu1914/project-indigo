5a18565b0858e59f138c8aa6963a5c2506c03f94
As-Harrie Dianalan <asharrie.dianalan@gmail.com>
2026-07-15 23:17:51 +0800

    feat(data): add sample data for Abilities

--- src/pokedex/data/Abilities.cpp
+++ src/pokedex/data/Abilities.cpp
@@ -12,16 +12,112 @@
 namespace indigo::abilities
 {
 
-const Ability BLAZE(
-    66,
-    "Blaze");
-
 const Ability OVERGROW(
-    65,
+    1,
     "Overgrow");
 
+const Ability CHLOROPHYLL(
+    2,
+    "Chlorophyll");
+
+const Ability BLAZE(
+    3,
+    "Blaze");
+
+const Ability SOLAR_POWER(
+    4,
+    "Solar Power");
+
 const Ability TORRENT(
-    67,
+    5,
     "Torrent");
+
+const Ability RAIN_DISH(
+    6,
+    "Rain Dish");
+
+const Ability SHIELD_DUST(
+    7,
+    "Shield Dust");
+
+const Ability RUN_AWAY(
+    8,
+    "Run Away");
+
+const Ability TINTED_LENS(
+    9,
+    "Tinted Lens");
+
+const Ability SWARM(
+    10,
+    "Swarm");
+
+const Ability SNIPER(
+    11,
+    "Sniper");
+
+const Ability KEEN_EYE(
+    12,
+    "Keen Eye");
+
+const Ability TANGLED_FEET(
+    13,
+    "Tangled Feet");
+
+const Ability BIG_PECKS(
+    14,
+    "Big Pecks");
+
+const Ability GUTS(
+    15,
+    "Guts");
+
+const Ability HUSTLE(
+    16,
+    "Hustle");
+
+const Ability INTIMIDATE(
+    17,
+    "Intimidate");
+
+const Ability STATIC(
+    18,
+    "Static");
+
+const Ability LIGHTNING_ROD(
+    19,
+    "Lightning Rod");
+
+const Ability SAND_VEIL(
+    20,
+    "Sand Veil");
+
+const Ability SAND_RUSH(
+    21,
+    "Sand Rush");
+
+const Ability POISON_POINT(
+    22,
+    "Poison Point");
+
+const Ability RIVALRY(
+    23,
+    "Rivalry");
+
+const Ability SHEER_FORCE(
+    24,
+    "Sheer Force");
+
+const Ability COMPOUND_EYES(
+    25,
+    "Compound Eyes");
+
+const Ability SHED_SKIN(
+    26,
+    "Shed Skin");
+
+const Ability UNNERVE(
+    27,
+    "Unnerve");
 
 } // end namespace indigo::types
