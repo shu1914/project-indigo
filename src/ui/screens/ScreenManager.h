28ed8c586d00f4cd9a0509350db0f878af692f96
As-Harrie Dianalan <asharrie.dianalan@gmail.com>
2026-07-20 00:23:26 +0800

    feat(screen): add ScreenManager

---
+++ src/ui/screens/ScreenManager.h
@@ -0,0 +1,56 @@
+/**
+ * @file ScreenManager.h
+ * @brief Screen Manager
+ * @author As-Harrie Dianalan (asharrie.dianalan@gmail.com)
+ * @date 2026-07-19
+ * 
+ * @copyright Copyright (c) 2026
+ */
+
+#ifndef SCREEN_MANAGER_H
+#define SCREEN_MANAGER_H
+
+#include <memory>
+#include <utility>
+#include "IScreen.h"
+
+namespace indigo
+{
+
+class ScreenManager
+{
+public:
+    ScreenManager() = default;
+    ~ScreenManager() = default;
+
+    template<typename T, typename... Args>
+    void show(Args&&... args);
+
+    IScreen* current() const
+    {
+        return _current.get();
+    }
+
+private:
+    std::unique_ptr<IScreen> _current;
+};
+
+template<typename T, typename... Args>
+void ScreenManager::show(Args&&... args)
+{
+    if (_current)
+    {
+        _current->onHide();
+    }
+
+    _current.reset();
+
+    _current = std::make_unique<T>(std::forward<Args>(args)...);
+
+    _current->onCreate();
+    _current->onShow();
+}
+
+}
+
+#endif // SCREEN_MANAGER_H
